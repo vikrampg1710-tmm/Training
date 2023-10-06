fecbfa809247315f6fce63474468800a6018b38e
Gopinath <Vikram.Gopinath@trumpf.com>
2023-10-06 12:36:06 +0530

    T1-Updated 1.0

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -4,16 +4,17 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using static System.ConsoleColor;
 
-namespace Training;
+namespace Spark;
 
 public class T1 {
    public static void Main () {
       List<int> testCases = new () { 0, 1, 4, 9, 10, 11, 16, 25, 36, 49, 50, 81, 100, 109, 125, 216, 343, 729, 1000, 12001, 66000, 123450, 1000909, 987654321, 100000001 };
       Console.WriteLine ("\x1B[4m" + "Decimal to Binary & Hexadecimal:" + "\x1B[0m");
       for (int i = 0; i < testCases.Count; i++) {
-         var item = Test[i];
+         var item = testCases[i];
          Console.WriteLine ($"\n{i + 1}) {item}: ");
          Console.Write ("Binary: ", 16);
          WriteInYellow (BinaryFormOf (item));
@@ -29,10 +30,27 @@ public class T1 {
       }
    }
    /// <summary>Converts input integer to a binary form</summary>
-   public static string BinaryFormOf (int num) => Convert.ToString (num, 2);
+   public static string BinaryFormOf (int num) {
+      if (num == 0) return "0";
+      string s = "";
+      while (num > 0) {
+         s = ((num % 2 == 0) ? "0" : "1") + s;
+         num /= 2;
+      }
+      return s;
+   }
 
    /// <summary>Converts input integer to a hexadecimal form</summary>
-   public static string HexaDecFormOf (int num) => Convert.ToString (num, 16);
+   public static string HexaDecFormOf (int num) {
+      if (num == 0) return "0";
+      string s = "";
+      while (num > 0) {
+         int a = num % 16;
+         s = ((a < 10) ? a : $"{(char)(87 + a)}") + s;
+         num /= 16;
+      }
+      return s;
+   }
 
    /// <summary>Writes input string in console with foreground colour as yellow</summary>
    public static void WriteInYellow (string output) {
