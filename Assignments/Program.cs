04c129b59ac397aa3d885b835914d9e76da5fcc9
Vikram P G <Vikram.Gopinath@trumpf.com>
2023-10-07 19:50:37 +0530

    T14-Updated 1.0

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -1,8 +1,7 @@
 ﻿// ---------------------------------------------------------------------------------------
 // T14 - Reduced String
 // ---------------------------------------------------------------------------------------
-
-using System;
+using System.Net.Http.Headers;
 using static System.ConsoleColor;
 namespace Spark;
 
@@ -11,25 +10,30 @@ public class T14 {
       Console.WriteLine ("\x1B[4m" + "String Reducer:-" + "\x1B[0m");
       Console.WriteLine ("Here, the input is a string, the output is a reduced string of lowercase characters by deleting a pair of adjacent letters that match. ");
       string[] testCases = { "aaabccddd", "aaaabbbb", "AAbcXXY", "xyz1233345667", "  " };
-      Console.WriteLine ("\n\x1B[4m" + "Test Cases:-" + "\x1B[0m");
+      Console.WriteLine ();
+      Console.WriteLine ("\x1B[4m" + "Test Cases:-" + "\x1B[0m");
       for (int i = 0; i < testCases.Length; i++) {
          Console.Write ($"\n{i + 1}. {testCases[i]} ==> ");
-         Console.WriteLine ($"{ReducedString (testCases[i])}", Console.ForegroundColor = Yellow);
+         Console.ForegroundColor = Yellow;
+         Console.WriteLine ($"{ReducedString (testCases[i])}");
          Console.ResetColor ();
       }
       Console.Write ("\nNow, let's input enter a string to reduce: ");
       string input = Console.ReadLine ();
-      Console.WriteLine ($"{ReducedString (input)}", Console.ForegroundColor = Yellow);
+      Console.ForegroundColor = Yellow;
+      Console.WriteLine ($"{ReducedString (input)}");
       Console.ResetColor ();
    }
+
    public static string ReducedString (string input) {
-      string output = "";
+      List<char> output = new ();
       for (int i = 0; i < input.Length; i++) {
-         if (output.Length == 0) { output += input[i]; continue; }
-         if (output[^1] != input[i]) output += input[i];
-         else output = output[..^1];
+         char c = input[i];
+         if (output.Count == 0) { output.Add (c); continue; }
+         if (output[^1] != c) output.Add (c);
+         else output.RemoveAt (output.Count - 1);
       }
-      return output;
+      return string.Join ("", output);
    }
 }
 
