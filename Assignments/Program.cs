2fc3503b911363c1e7a8c0b19124fceb6121a6a7
Vikram <vikram.pg@metamation.com>
2023-10-09 21:24:15 +0530

    T15 - Updated 1.0

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -1,9 +1,10 @@
 ﻿// ---------------------------------------------------------------------------------------
-// T13 - Strong Password
+// T15 - Isogram
 // ---------------------------------------------------------------------------------------
 using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
+using System.Globalization;
 using static System.ConsoleColor;
 
 namespace Spark;
@@ -12,35 +13,34 @@ public class T15 {
    public static void Main () {
       Console.WriteLine ("\x1B[4m" + "Check for Isogram Word:-" + "\x1B[0m");
       Console.WriteLine ("An isogram is a word that has no duplicate letters.");
-      string output;
-      List<string> testCases = new () { "apple", "banana", "cat", "doctrine", "elephant", "fruits", "pebble", "rubiks", "yellow", "zebra" };
+      List<string> testCases = new () { "", "apple", "banana", "cat", "doctrine", "elephant", "fruits", "pebble", "rubiks", "yellow", "zebra", "12345", "123345", "@#$4%! -)", "*765^%$*" };
       Console.WriteLine ();
       Console.WriteLine ("\x1B[4m" + "Test Cases:" + "\x1B[0m");
       for (int i = 0; i < testCases.Count; i++) {
-         Console.Write ($"{i + 1}. {testCases[i]} - ");
-         (output, Console.ForegroundColor) = IsIsogram (testCases[i]) ? ("Isogram", Green) : ("Not an Isogram", Red);
-         Console.WriteLine ($"{output}");
-         Console.ResetColor ();
+         var t = testCases[i];
+         Console.Write ($"{i + 1, 2}. {t, 11} - ");
+         perform (t);
       }
       Console.Write ("\nNow, let's enter a word: ");
-      string input = Console.ReadLine ().ToUpper ();
-      (output, Console.ForegroundColor) = IsIsogram (input) ? ("Isogram", Green) : ("Not an Isogram", Red);
+      perform (Console.ReadLine ());
+   }
+
+   /// <summary>Prints the result whether the input is isogram or not</summary>
+   public static void perform (string input) {
+      (string output, Console.ForegroundColor) = IsIsogram (input) ? ("Isogram", Green) : ("Not an Isogram", Red);
       Console.WriteLine ($"{output}");
       Console.ResetColor ();
    }
 
    /// <summary>Returns true if input string is an isogram, otherwise false</summary>
    public static bool IsIsogram (string input) {
+      if (input.Length == 0) return false;
       input = input.ToUpper ();
-      bool isIsogram = true;
-      if (input.All (char.IsLetter)) {
-         List<int> HashTable = new ();
-         for (int j = 0; j < 26; j++) HashTable.Add (0);
-         foreach (char c in input) {
-            if (HashTable[c - 65] == 0) HashTable[c - 65]++;
-            else isIsogram = false;
-         }
+      Dictionary<char, int> freq = new ();
+      foreach (char c in input) {
+         if (freq.TryGetValue (c, out int _)) return false;
+         else freq.Add (c, 1);
       }
-      return isIsogram;
+      return true;
    }
 }
