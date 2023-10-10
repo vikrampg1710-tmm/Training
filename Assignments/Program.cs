ffcf065907d40369104f58b8e0a5211f2a60efd5
Vikram <vikram.pg@metamation.com>
2023-10-10 19:49:48 +0530

    T14-Updated 2.0

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -1,30 +1,27 @@
 ﻿// ---------------------------------------------------------------------------------------
 // T14 - Reduced String
 // ---------------------------------------------------------------------------------------
-using System.Net.Http.Headers;
-using static System.ConsoleColor;
+using System;
+using System.Collections.Generic;
 namespace Spark;
 
 public class T14 {
    public static void Main () {
       Console.WriteLine ("\x1B[4m" + "String Reducer:-" + "\x1B[0m");
       Console.WriteLine ("Here, the input is a string, the output is a reduced string of lowercase characters by deleting a pair of adjacent letters that match. ");
-      string[] testCases = { "aaabccddd", "aaaabbbb", "AAbcXXY", "xyz1233345667", "  " };
+      string[] testCases = { "  ", "aaabccddd", "aaaabbbb", "AAbcXXY", "xyz1233345667" };
       Console.WriteLine ();
       Console.WriteLine ("\x1B[4m" + "Test Cases:-" + "\x1B[0m");
+      Console.WriteLine ();
       for (int i = 0; i < testCases.Length; i++) {
-         Console.Write ($"\n{i + 1}. {testCases[i]} ==> ");
-         Console.ForegroundColor = Yellow;
-         Console.WriteLine ($"{ReducedString (testCases[i])}");
-         Console.ResetColor ();
+         Console.Write ($"{i + 1}. {testCases[i], 15} ==> ");
+         PrintInBlue (ReducedString (testCases[i]));
       }
       Console.Write ("\nNow, let's input enter a string to reduce: ");
-      string input = Console.ReadLine ();
-      Console.ForegroundColor = Yellow;
-      Console.WriteLine ($"{ReducedString (input)}");
-      Console.ResetColor ();
+      PrintInBlue (ReducedString (Console.ReadLine ()));
    }
 
+   /// <summary>Returns the reduced string by deleting the pairs of characters if any</summary>
    public static string ReducedString (string input) {
       List<char> output = new ();
       for (int i = 0; i < input.Length; i++) {
@@ -33,7 +30,14 @@ public class T14 {
          if (output[^1] != c) output.Add (c);
          else output.RemoveAt (output.Count - 1);
       }
-      return string.Join ("", output);
+      return new string (output.ToArray());
+   }
+
+   /// <summary>Prints the input string with Foreground colour as blue</summary>
+   public static void PrintInBlue (string input) {
+      Console.ForegroundColor = ConsoleColor.Blue;
+      Console.WriteLine (input);
+      Console.ResetColor ();
    }
 }
 
