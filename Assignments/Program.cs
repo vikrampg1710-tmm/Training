b7dcc40fc30fa54175e5f8b7104fe4c1e43fabaa
Vikram <vikram.pg@metamation.com>
2023-10-11 14:40:45 +0530

    T4 - Updated 2.0

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -1,7 +1,10 @@
 ﻿// ---------------------------------------------------------------------------------------
+// Spark23 Assignments
+// Copyright (c) Metamation India.
+// ---------------------------------------------------------------------------------------
+// Program.cs
 // T4 - GCD & LCM Computer
 // ---------------------------------------------------------------------------------------
-
 using System;
 using System.Collections.Generic;
 using static System.ConsoleColor;
@@ -14,10 +17,12 @@ public class T4 {
       List<List<int>> testCases = new () { new () { 5, 10, 15, 21 }, new () { 6, 18, 36, 54, 96 }, 
          new () { 6, 8, 12, 10, 30, 72, 134 }, new () { 2, 3, 5, 7, 11, 13 }, new () { 17, 34, 51, 68, 36 } };
       for (int i = 0; i < testCases.Count; i++) {
-         Console.Write ($"\nTest case-{i + 1}: ");
+         Console.WriteLine ();
+         Console.Write ($"Test case-{i + 1}: ");
          PrintResult (testCases[i]);
       }
-      Console.Write ("\nNow, lets try. Enter the count of no. of inputs (2 - 10): ");
+      Console.WriteLine ();
+      Console.Write ("Now, lets try. Enter the count of no. of inputs (2 - 10): ");
       if (int.TryParse (Console.ReadLine (), out int n) && n > 1 && n < 11) {
          List<int> inputs = new ();
          for (int i = 0; i < n; i++) {
@@ -29,7 +34,8 @@ public class T4 {
          PrintResult (inputs);
       } else {
          Console.ForegroundColor = Red;
-         Console.WriteLine ("\nProgram terminated due to incorrect input.");
+         Console.WriteLine ();
+         Console.WriteLine ("Program terminated due to incorrect input.");
          Console.ResetColor ();
       }
    }
@@ -43,27 +49,24 @@ public class T4 {
          if (i != len - 1) Console.Write (", ");
       }
       Console.ResetColor ();
-      Console.Write ("\nGCD = "); WriteInYellow ($"{GCDof (input)}");
+      Console.WriteLine ();
+      Console.Write ("GCD = "); WriteInYellow ($"{GCDof (input)}");
       Console.Write ("LCM = "); WriteInYellow ($"{LCMof (input)}");
    }
 
    /// <summary>Returns the Greatest Common Factor of all the numbers in the input list</summary>
    public static int GCDof (List<int> inputs) {
       int result = EuclidGCDof (inputs[0], inputs[1]);
-      for (int i = 2; i < inputs.Count; i++) {
-         var temp = EuclidGCDof (result, inputs[i]);
-         result = temp;
-      }
+      for (int i = 2; i < inputs.Count; i++)
+         result = EuclidGCDof (result, inputs[i]);
       return result;
    }
    
    /// <summary>Returns the Least Common Multiple of all numbers in the input list</summary>
    public static int LCMof (List<int> inputs) {
       int result = EuclidLCMof (inputs[0], inputs[1]);
-      for (int i = 2; i < inputs.Count; i++) {
-         int temp = EuclidLCMof (result, inputs[i]);
-         result = temp;
-      }
+      for (int i = 2; i < inputs.Count; i++)
+         result = EuclidLCMof (result, inputs[i]);
       return result;
    }
 
