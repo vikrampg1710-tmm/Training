d53e38dc79f61745d4248d2db3398e4e5bc11ea7
Vikram P G <Vikram.Gopinath@trumpf.com>
2023-10-14 21:10:33 +0530

    Minor changes done

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -3,7 +3,8 @@
 // ---------------------------------------------------------------------------------------
 
 using System;
-using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
 using static System.ConsoleColor;
 
 namespace Spark;
@@ -19,7 +20,7 @@ public class T2 {
          WriteInYellow (ToWords (item));
          Console.ResetColor ();
       }
-      Console.WriteLine ("\n\x1B[4m" + "Numbers to Romans:" + "\x1B[0m");
+      Console.WriteLine ("\nthDigit\x1B[4m" + "Numbers to Romans:" + "\x1B[0m");
       for (int i = 0; i < rTestCases.Count; i++) {
          var item = rTestCases[i];
          Console.Write ($"{i + 1,2}) {item,4} ==> ");
@@ -28,7 +29,7 @@ public class T2 {
       }
    }
 
-   /// <summary>Converts input number to equivalent words</summary>
+   /// <summary>Retuns the input number in words using State Machine</summary>
    public static string ToWords (int num) {
       if (num == 0) return "Zero";
       string input = num.ToString ().PadLeft (9, '0'),
@@ -36,36 +37,45 @@ public class T2 {
       List<string> ones = new () { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
       "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" },
                    tens = new () { "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
+      // As this methods works till 9-digit number, this first loop segregates the input (string) into 3 + 3 + 3 digits, and parse into integer.
       for (int i = 0; i < 3; i++) {
+         // Here part is FIRST 3-digits for (i = 0), SECOND 3-digits for (i = 1), and LAST 3-digits for (i = 2).
+         // Eg: num = 123456 ==> input = 000123456.  Then, part = 000 (for i = 0); 123 (for i = 1); 456 (for i = 2).
          int part = int.Parse (input.Substring (i * 3, 3));
-         State s = State.A;
-         for (int loop = 3; loop > 0; loop--) {
-            int n = part / (int)Math.Pow (10, loop - 1) % 10, d3 = part % 10;
-            switch (s, n) {
-               case (State.A, >= 0 and <= 9): {
-                     s = State.B;
-                     if (n != 0) {
-                        output += ones[n] + " Hundred ";
-                        if (part / 10 % 10 > 0 || d3 > 0) output += "and ";
+         // For any 3-digit number, the place value of first digit is always Hundreds.  So the state s always starts with Hundreds.
+         State s = State.Hundreds;
+         // Below loop is used to take out digits ONE BY ONE from part and converted into words.
+         for (int n = 3; n > 0; n--) {
+            // nthDigit is the 3rd digit of part for (n = 3), and so on.
+            // onesDigit is the always the 3rd digit of part.
+            int nthDigit = part / (int)Math.Pow (10, n - 1) % 10, onesDigit = part % 10;
+            // The below switch cases is implemented based on the STATE MACHINE DIAGRAM attached.
+            switch (s, nthDigit) {
+               case (State.Hundreds, >= 0 and <= 9): {
+                     s = State.Tens;
+                     if (nthDigit != 0) {
+                        output += ones[nthDigit] + " Hundred ";
+                        if (part / 10 % 10 > 0 || onesDigit > 0) output += "and ";
                      }
                      break;
                   }
-               case (State.B, 1): {
-                     s = State.D;
-                     output += ones[10 + d3] + " ";
+               case (State.Tens, 1): {
+                     s = State.End;
+                     output += ones[10 + onesDigit] + " ";
                      break;
                   }
-               case (State.B, 0 or (>= 2 and <= 9)): {
-                     s = State.C;
-                     if (n != 0) output += tens[n] + ((i == 2 && d3 > 0) ? "-" : " ");
+               case (State.Tens, 0 or (>= 2 and <= 9)): {
+                     s = State.Ones;
+                     if (nthDigit != 0) output += tens[nthDigit] + ((i == 2 && onesDigit > 0) ? "-" : " ");
                      break;
                   }
-               case (State.C, >= 0 and <= 9): {
-                     s = State.D;
-                     if (n != 0) output += ones[n] + " ";
+               case (State.Ones, >= 0 and <= 9): {
+                     s = State.End;
+                     if (nthDigit != 0) output += ones[nthDigit] + " ";
                      break;
                   }
-               default: { s = State.D; break; }
+               default: { s = State.End; break; }
             }
          }
          if (part > 0 && i != 2) {
@@ -75,16 +85,16 @@ public class T2 {
       }
       return output;
    }
-   enum State { A, B, C, D };
+   enum State { Hundreds, Tens, Ones, End };
    #region State Machine Diagram for converting any 3-digit Number to Words:
    /*
-    *                                  if (n == 0) -->                          if (n == 0) --->                         if (n == 0) --->
+    *                                  if (nthDigit == 0) -->                          if (nthDigit == 0) --->                         if (nthDigit == 0) --->
     *                             ┌------------>----------┐                ┌----------->-----------┐                ┌----------->-----------┐
     *                             ↑                       ↓                ↑                       ↓                ↑                       ↓
     *                             │    else --------->    │                │    else ---------->   │                │    else ---------->   │
-    *  o---->----A(n = 0...9)-->--⬛--->---[action1]--->---B(n = 0...9)-->--⬛--->---[action3]--->---C(n = 0...9)-->--⬛--->---[action4]--->---⬛-----D       
+    *  o---->----Hundreds(nthDigit = 0...9)-->--⬛--->---[action1]--->---Tens(nthDigit = 0...9)-->--⬛--->---[action3]--->---Ones(nthDigit = 0...9)-->--⬛--->---[action4]--->---⬛-----End       
     *                                                                      │                                                                │    
-    *                                                                      ↓     if (n == 1) -->                                            ↑
+    *                                                                      ↓     if (nthDigit == 1) -->                                            ↑
     *                                                                      └----------->---------------[action2]---------------->-----------┘
     *  
     *  Where,
@@ -108,7 +118,8 @@ public class T2 {
    }
 
    public static void WriteInYellow (string output) {
-      Console.WriteLine (output, Console.ForegroundColor = Yellow);
+      Console.ForegroundColor = Yellow;
+      Console.WriteLine (output);
       Console.ResetColor ();
    }
 }
