caf089f0a79bf8c0315c6726e9823267797c88a7
Vikram P G <Vikram.Gopinath@trumpf.com>
2023-10-17 18:28:51 +0530

    T2 - Review 1

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -1,4 +1,8 @@
 ﻿// ---------------------------------------------------------------------------------------
+// Spark23 Assignments
+// Copyright (c) Metamation India.
+// ---------------------------------------------------------------------------------------
+// Program.cs
 // T2 - NUMBER TO WORDS AND ROMAN NUMERALS CONVERTER 
 // ---------------------------------------------------------------------------------------
 
@@ -11,22 +15,24 @@ namespace Spark;
 
 public class T2 {
    public static void Main () {
-      List<int> wTestCases = new () { 0, 1, 4, 9, 10, 11, 16, 25, 36, 49, 50, 81, 100, 109, 125, 216, 343, 729, 1000, 12001, 66000, 123450, 1000909, 987654321, 100000001 };
-      List<int> rTestCases = new () { 1, 4, 5, 9, 10, 47, 94, 100, 345, 400, 499, 500, 900, 998, 1000, 1500, 1729, 1888, 3495, 3913, 4000, 4096, 4509, 4761, 4999 };
+      List<int> wTestCases = new () { 0, 1, 4, 9, 10, 11, 16, 25, 36, 49, 50, 81, 100, 109, 125, 216,
+         343, 729, 1000, 12001, 66000, 123450, 1000909, 987654321, 100000001 };
+      List<int> rTestCases = new () { 1, 4, 5, 9, 10, 47, 94, 100, 345, 400, 499, 500, 900, 998,
+         1000, 1500, 1729, 1888, 3495, 3913, 4000, 4096, 4509, 4761, 4999 };
       Console.WriteLine ("\x1B[4m" + "Numbers to Words:" + "\x1B[0m");
-      for (int i = 0; i < wTestCases.Count; i++) {
-         var item = wTestCases[i];
-         Console.Write ($"{i + 1,2}) {item,9} ==> ");
-         WriteInYellow (ToWords (item));
-         Console.ResetColor ();
-      }
-      Console.WriteLine ("\nthDigit\x1B[4m" + "Numbers to Romans:" + "\x1B[0m");
-      for (int i = 0; i < rTestCases.Count; i++) {
-         var item = rTestCases[i];
-         Console.Write ($"{i + 1,2}) {item,4} ==> ");
-         WriteInYellow (ToRomans (item));
-         Console.ResetColor ();
-      }
+      for (int i = 0; i < wTestCases.Count; i++)
+         PrintResult (i, wTestCases[i], true);
+      Console.WriteLine ("\n\x1B[4m" + "Numbers to Romans:" + "\x1B[0m");
+      for (int i = 0; i < rTestCases.Count; i++)
+         PrintResult (i, rTestCases[i], false);
+   }
+
+   /// <summary>Prints the words or roman numerals equivalent to the input number</summary>
+   public static void PrintResult (int index, int input, bool toWords) {
+      Console.Write ($"{index + 1,2}) {input,10} ==> ");
+      Console.ForegroundColor = Yellow;
+      Console.WriteLine (toWords ? ToWords (input) : ToRomans (input));
+      Console.ResetColor ();
    }
 
    /// <summary>Retuns the input number in words using State Machine</summary>
@@ -50,7 +56,7 @@ public class T2 {
             // nthDigit is the 3rd digit of part for (n = 3), and so on.
             // onesDigit is the always the 3rd digit of part.
             int nthDigit = part / (int)Math.Pow (10, n - 1) % 10, onesDigit = part % 10;
-            // The below switch cases is implemented based on the STATE MACHINE DIAGRAM attached.
+            // The below switch cases is implemented based on the STATE MACHINE DIAGRAM attached in "Data".
             switch (s, nthDigit) {
                case (State.Hundreds, >= 0 and <= 9): {
                      s = State.Tens;
@@ -86,41 +92,18 @@ public class T2 {
       return output;
    }
    enum State { Hundreds, Tens, Ones, End };
-   #region State Machine Diagram for converting any 3-digit Number to Words:
-   /*
-    *                                  if (nthDigit == 0) -->                          if (nthDigit == 0) --->                         if (nthDigit == 0) --->
-    *                             ┌------------>----------┐                ┌----------->-----------┐                ┌----------->-----------┐
-    *                             ↑                       ↓                ↑                       ↓                ↑                       ↓
-    *                             │    else --------->    │                │    else ---------->   │                │    else ---------->   │
-    *  o---->----Hundreds(nthDigit = 0...9)-->--⬛--->---[action1]--->---Tens(nthDigit = 0...9)-->--⬛--->---[action3]--->---Ones(nthDigit = 0...9)-->--⬛--->---[action4]--->---⬛-----End       
-    *                                                                      │                                                                │    
-    *                                                                      ↓     if (nthDigit == 1) -->                                            ↑
-    *                                                                      └----------->---------------[action2]---------------->-----------┘
-    *  
-    *  Where,
-    *    action1: output += (one/two/.../eight/nine) + "Hundred and"
-    *    action2: output += (eleven/twelve/.../eighteen/nineteen)
-    *    action3: output += (ten/eleven/.../eighty/ninety)
-    *    action4: output += (one/two/.../eight/nine)
-    */
-   #endregion
 
    /// <summary>Converts input number to Roman Numerals</summary>
    public static string ToRomans (int num) {
-      string output = "";
-      var romans = new char[7] { 'M', 'D', 'C', 'L', 'X', 'V', 'I' };
-      for (int i = 0, r = 1000; i < 7; i++) {
-         output += new string (romans[i], num / r);
-         num %= r;
-         r /= (i % 2 == 0) ? 2 : 5;
+      List<string> output = new ();
+      int[] value = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+      string[] roman = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+      for (int i = 0; i < value.Length; i++) {
+         int x = value[i];
+         output.Add (string.Concat (Enumerable.Repeat (roman[i], num / x)));
+         num %= x;
       }
-      return output.Replace ("VIIII", "IX").Replace ("IIII", "IV").Replace ("LXXXX", "XC").Replace ("XXXX", "XL").Replace ("DCCCC", "CM").Replace ("CCCC", "CD");
-   }
-
-   public static void WriteInYellow (string output) {
-      Console.ForegroundColor = Yellow;
-      Console.WriteLine (output);
-      Console.ResetColor ();
+      return string.Join ("", output);
    }
 }
 
