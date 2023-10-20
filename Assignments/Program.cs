0085458d32a69dd8b22cd1331f75f555672fb14e
Vikram P G <Vikram.Gopinath@trumpf.com>
2023-10-20 20:01:15 +0530

    T02: Review fix

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -18,7 +18,7 @@ public class T2 {
    public static void Main () {
       List<int> wTestCases = new () { 0, 1, 4, 9, 10, 11, 16, 25, 36, 49, 50, 81, 100, 109, 125, 216,
                                     343, 729, 1000, 12001, 66000, 123450, 1000909, 987654321, 100000001 },
-                rTestCases = new () { 0, 1, 4, 5, 9, 10, 47, 94, 100, 345, 400, 499, 500, 900, 998, 1000, 
+                rTestCases = new () { 0, 1, 4, 5, 9, 10, 47, 94, 100, 345, 400, 499, 500, 900, 998, 1000,
                                     1500, 1729, 1888, 3495, 4000, 3999 };
       Console.WriteLine ("\x1B[4m" + "Numbers to Words:" + "\x1B[0m");
       PrintResult (wTestCases, true);
@@ -32,7 +32,7 @@ public class T2 {
          int n = input[i];
          Console.Write ($"{i + 1,2}) {input[i],10} ==> ");
          if (!toWords && n > 3999) {
-            Console.WriteLine ($"Invalid Input!");
+            Console.WriteLine ($"Invalid input!");
             continue;
          }
          Console.ForegroundColor = Green;
@@ -44,7 +44,7 @@ public class T2 {
    /// <summary>Returns the input number in words using State Machine</summary>
    public static string ToWords (int num) {
       if (num == 0) return "Zero";
-      string input = num.ToString ().PadLeft (9, '0'), 
+      string input = num.ToString ().PadLeft (9, '0'),
             output = "";
       List<string> ones = new () { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
       "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" },
@@ -56,12 +56,12 @@ public class T2 {
          3 digit integer, and made run into the second for-loop.
          Eg: num = 123456, then input = "000123456" => part = { 000 (for i = 0); 123 (for i = 1); 456 (for i = 2) }
       
-      2. In the SECOND for-loop, each single digits from 'part' stored in 'nthDigit' is taken out one by one and made 
+      2. In the SECOND for-loop, each single digits from 'part' stored in 'nthDigit' is taken out one by one and made
          run through the state machine.
-         Eg: part = 123 => nthDigit = { 3 (for n = 3); 2 (for n = 2); 1 (for n = 1) }
+         Eg: part = 123 => nthDigit = { 1 (for n = 3); 2 (for n = 2); 3 (for n = 1) }
              thirdDigit = 3 (3rd digit of part)
 
-      3. The state machine starts with the state of HUNDREDS as the maximum place value of any 3-digit number is 100.
+      3. The state machine starts with the state of HUNDREDS as the maximum place values of any 3-digit number is 100.
      
       Overall, the first loop segregates the input string into three 3-digit numbers and supplies to the second loop,
       which converts the supplied 3-digit number to equivalent words.  Finally all the words are added together and returned.
@@ -69,7 +69,8 @@ public class T2 {
 
       // FIRST for-loop:
       for (int i = 0; i < 3; i++) {
-         int part = int.Parse (input.Substring (i * 3, 3));
+         int part = int.Parse (input[(i * 3)..((i + 1) * 3)]);
+         if (part == 0) continue;
          State s = State.Hundreds;
          // SECOND for-loop:
          for (int n = 3; n > 0; n--) {
@@ -116,16 +117,15 @@ public class T2 {
       StringBuilder output = new ();
       if (num == 0) output.Append ("Zero has no symbol in Roman numerals system.");
       else {
-         int[] value = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
-         string[] roman = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-         int v = Array.Find (value, a => a <= num);
-         for (int i = Array.IndexOf (value, v); i < value.Length; i++) {
-            int x = value[i], y = num / x;
-            output.Append (string.Concat (Enumerable.Repeat (roman[i], y)));
+         int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+         string[] romans = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+         int start = Array.IndexOf (values, Array.Find (values, a => a <= num));
+         for (int i = start; i < values.Length && num != 0; i++) {
+            int x = values[i], y = num / x;
+            output.Append (string.Concat (Enumerable.Repeat (romans[i], y)));
             num %= x;
          }
       }
       return output;
    }
 }
-
