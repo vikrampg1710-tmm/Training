23ae4de4548ff89aaf10b29d8eb59c94181c02cb
Vikram P G <Vikram.Gopinath@trumpf.com>
2023-11-06 15:47:58 +0530

    T19: Review fix

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -8,7 +8,6 @@
 
 using System;
 using System.Collections.Generic;
-using static System.ConsoleColor;
 
 namespace Spark;
 public class T19 {
@@ -17,9 +16,8 @@ public class T19 {
       List<int> testCases = new () { 0, 1, 2, 3, 5, 8, 10, 12, 15, 16 };
       Console.WriteLine ("Test Cases:");
       for (int i = 0; i < testCases.Count; i++) {
-         int t = testCases[i];
          Console.Write ($"{i + 1,2}.  ");
-         PrintResult (t);
+         PrintResult (testCases[i]);
       }
       Console.WriteLine ();
       Console.Write ("Now, let's try out.  Enter a number between 0 - 15, n = ");
@@ -29,17 +27,9 @@ public class T19 {
    /// <summary>Print the results in the console page</summary>
    public static void PrintResult (int input) {
       Console.Write ($"{input}! = ");
-      Console.ForegroundColor = Green;
+      Console.ForegroundColor = ConsoleColor.Green;
       Console.WriteLine (RFactorialOf (input));
       Console.ResetColor ();
-   }
-
-   /// <summary>Returns the Factorial value of given input - WITHOUT RECURSION</summary>
-   public static int FactorialOf (int n) {
-      if (n == 0) return 1;
-      int ans = 1;
-      for (int i = n; i > 0; i--) ans *= i;
-      return ans;
    }
 
    /// <summary>Returns the Factorial value of given input - WITH RECURSION</summary>
@@ -48,8 +38,6 @@ public class T19 {
       if (n == 1 || n == 2) return n;
       return n * RFactorialOf (n - 1);
    }
-
-
 }
 
 
