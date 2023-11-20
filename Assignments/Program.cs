1ea9434618e782e13458a591a6309a852506cd56
Vikram P G <Vikram.Gopinath@trumpf.com>
2023-11-20 10:29:19 +0530

    T21: Review fix

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -3,7 +3,7 @@
 // Copyright (c) Metamation India.
 // ---------------------------------------------------------------------------------------
 // Program.cs
-// T20 - Swap Two Numbers
+// T21 - Swap Two Numbers
 // ---------------------------------------------------------------------------------------
 
 using System;
@@ -17,10 +17,15 @@ public class T21 {
       Console.Write ("Enter Number, b = ");
       int b = Convert.ToInt32 (Console.ReadLine ());
       (a, b) = Swap (a, b);
-      Console.WriteLine ($"\r\nAfter Swaping: \r\na = {a}\r\nb = {b}");
+      Console.WriteLine (value: $"\r\nAfter Swapping: \r\na = {a}\r\nb = {b}");
    }
 
    /// <summary>Swaps the given two numbers</summary>
-   public static (int, int) Swap (int a, int b) => (b, a);
+   public static (int, int) Swap (int a, int b) {
+      int temp = a;
+      a = b; 
+      b = temp;
+      return (a, b);
+   }
 }
 
