9b31660b6b82a691cffdd195cda8de7945ba333c
Vikram P G <Vikram.Gopinath@trumpf.com>
2023-11-29 12:05:04 +0530

    T27: Review fix - Comments added & MSTest file updated

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -7,36 +7,50 @@
 // ---------------------------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Spark23;
 
 public class T27 {
    public static void Main () {
-      var stack = new TStack<int> ();
-      stack.Push (1);
-      stack.Peek ();
-      stack.Pop ();
+      var st = new Stack<int> ();
    }
+
+   #region Custom Generic Class of Stack: TStack<T>
+   /// <summary>Initiates a new instance of the TStack<typeparamref name="T"/> that is empty and has the default initial capacity</summary>
    public class TStack<T> {
+
+      /// <summary>Returns true if the stack is empty</summary>
       public bool IsEmpty => mIndex == 0;
-      public int Size => mIndex;
-      public void Push (T value) {
-         if (mIndex == mStack.Length)
-            Array.Resize (ref mStack, mIndex * 2);
-         mStack[mIndex++] = value;
+
+      /// <summary>Gets the number of elements contained in the stack</summary>
+      public int Count => mIndex;
+
+      /// <summary>Returns the object on top of the TStack<typeparamref name="T"/> without removing it</summary>
+      /// <returns>The object removed from the top of the TStack<typeparamref name="T"/></returns>
+      /// <exception cref="InvalidOperationException"></exception>
+      public T Peek () {
+         if (IsEmpty) throw new InvalidOperationException ("Stack Empty");
+         return mStack[mIndex - 1];
       }
 
+      /// <summary>Removes and returns the object on top of the TStack<typeparamref name="T"/></summary>
+      /// <returns>The object removed from the top of the TStack<typeparamref name="T"/></returns>
+      /// <exception cref="InvalidOperationException"></exception>
       public T Pop () {
          if (IsEmpty) throw new InvalidOperationException ("Stack Empty");
          return mStack[--mIndex];
       }
 
-      public T Peek () {
-         if (IsEmpty) throw new InvalidOperationException ("Stack Empty");
-         var x = mIndex - 1;
-         return mStack[x];
+      /// <summary>Inserts an object on top of the TStack<typeparamref name="T"/></summary>
+      public void Push (T value) {
+         if (mIndex == mStack.Length)
+            Array.Resize (ref mStack, mIndex * 2);
+         mStack[mIndex++] = value;
       }
-
       T[] mStack = new T[4];
       int mIndex = 0;
    }
+   #endregion
 }
--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -4,43 +4,72 @@ namespace Test;
 
 [TestClass]
 public class UnitTest1 {
+   Random r = new ();
    [TestMethod]
    ///<summary>Checking the functioning of PUSH method</summary>
-   public void TestMethod1 () {
-      TStack<int> stack = new ();
-      Assert.IsTrue (stack.IsEmpty);
-      for (int i = 0; i < 10; i++) {
-         Assert.AreEqual (stack.Size, i);
-         stack.Push (i);
+   public void PushTest () {
+      TStack<int> stack1 = new ();
+      Stack<int> stack2 = new ();
+
+      Assert.IsTrue (stack1.IsEmpty);
+      for (int i = 0; i < 50; i++) {
+         bool push = r.Next (0, 2) == 1;
+         if (push) {
+            var (num1, num2) = (r.Next (0, 100), r.Next (0, 100));
+            stack1.Push (num1); stack1.Push (num2);
+            stack2.Push (num1); stack2.Push (num2);
+         } else if (!stack1.IsEmpty) {
+            stack1.Pop ();
+            stack2.Pop ();
+         }
+         Assert.IsTrue (stack1.Count == stack2.Count);
       }
-      Assert.IsFalse (stack.IsEmpty);
-      Assert.IsTrue (stack.Size == 10);
+      for (int x = 0; x < stack1.Count; x++)
+         Assert.AreEqual (stack1.Pop (), stack2.Pop ());
    }
 
    [TestMethod]
    ///<summary>Check the functioning of POP method</summary>
-   public void TestMethod2 () {
-      TStack<int> stack = new ();
-      for (int i = 0; i < 10; i++) stack.Push (i);
-      for (int i = 9; i >= 0; i--) {
-         Assert.AreEqual (stack.Size, i + 1);
-         Assert.IsTrue (stack.Pop () == i);
+   public void PopTest () {
+      TStack<int> stack1 = new ();
+      Stack<int> stack2 = new ();
+      for (int i = 0; i < 50; i++) {
+         bool push = r.Next (0, 2) == 1;
+         if (push) {
+            int num1 = r.Next (0, 100);
+            stack1.Push (num1);
+            stack2.Push (num1);
+         } else {
+            if (stack1.IsEmpty) Assert.ThrowsException<InvalidOperationException> (() => stack1.Pop ());
+            else {
+               stack1.Pop ();
+               stack2.Pop ();
+            }
+         }
       }
-      Assert.IsTrue (stack.IsEmpty);
-      Assert.ThrowsException<InvalidOperationException> (() => stack.Pop ());
+      for (int x = 0; x < stack1.Count; x++)
+         Assert.AreEqual (stack1.Pop (), stack2.Pop ());
    }
 
    [TestMethod]
    ///<summary>Check the functioning of PEEK method</summary>
-   public void TestMethod3 () {
-      TStack<int> stack = new ();
-      for (int i = 0; i < 10; i++) stack.Push (i);
-      Assert.IsFalse (stack.IsEmpty);
-      for (int i = 9; i >= 0; i--) {
-         Assert.IsTrue (stack.Peek () == i);
-         stack.Pop ();
+   public void PeekTest () {
+      TStack<int> stack1 = new ();
+      Stack<int> stack2 = new ();
+      for (int i = 0; i < 50; i++) {
+         bool push = r.Next (0, 2) == 1;
+         if (push) {
+            int num1 = r.Next (0, 100);
+            stack1.Push (num1);
+            stack2.Push (num1);
+         } else if (!stack1.IsEmpty) {
+            stack1.Pop ();
+            stack2.Pop ();
+         }
+         if (stack1.IsEmpty) Assert.ThrowsException<InvalidOperationException> (() => stack1.Peek ());
+         else Assert.AreEqual (stack1.Peek (), stack2.Peek ());
       }
-      Assert.IsTrue (stack.IsEmpty);
-      Assert.ThrowsException<InvalidOperationException> (() => stack.Peek ());
+      for (int x = 0; x < stack1.Count; x++)
+         Assert.AreEqual (stack1.Pop (), stack2.Pop ());
    }
 }
