d8a7a2a5ce3dc275c6991aa7b5881d010ddd24d6
Vikram P G <Vikram.Gopinath@trumpf.com>
2023-12-01 19:12:57 +0530

    A14.1: Code updated

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -15,27 +15,27 @@ using System.Diagnostics;
 namespace Academy;
 public class A14 {
    public static void Main () {
-      var DictWords = new List<string> (File.ReadAllLines (@"c:/etc/words.txt"));
-      Dictionary<string, List<string>> Anagrams = new ();
+      var wordList = new List<string> (File.ReadAllLines (@"c:/etc/words.txt"));
+      Dictionary<string, List<string>> anagrams = new ();
       int count = 0;
-      List<int> HashTable;
+      int[] hashTable = new int[26];
       var watch = Stopwatch.StartNew (); //.............................TIMER ON
-      foreach (string A in DictWords) {
-         string Hash = HashTableOf (A);
-         if (Anagrams.TryGetValue (Hash, out List<string> value)) value.Add (A);
-         else Anagrams.Add (Hash, new List<string> { A });
+      foreach (string s in wordList) {
+         string Hash = HashCode (ref hashTable, s);
+         if (anagrams.TryGetValue (Hash, out List<string> value)) value.Add (s);
+         else anagrams.Add (Hash, new List<string> { s });
       }
       watch.Stop ();//..................................................TIMER OFF
       Console.WriteLine ("\x1B[4m" + "Anagrams:-" + "\x1B[0m");
-      foreach (KeyValuePair<string, List<string>> abc in Anagrams.Where (abc => abc.Value.Count >= 2).OrderByDescending (a => a.Value.Count))
-         Console.WriteLine ($"{++count,4}. {abc.Value.Count,2}- {string.Join (", ", abc.Value)}");
+      foreach (var kvp in anagrams.Where (a => a.Value.Count > 1).OrderByDescending (a => a.Value.Count))
+         Console.WriteLine ($"{++count,4}. {kvp.Value.Count,2}- {string.Join (", ", kvp.Value)}");
       Console.WriteLine ($"\nThe Execution time of the program: {watch.ElapsedMilliseconds} ms");
 
       /// <summary>Returns an unique string made of hash-table digits of given input string</summary>
-      string HashTableOf (string word) {
-         HashTable = new () { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-         foreach (char c in word) HashTable[c - 65]++;
-         return string.Join ("", HashTable);
+      static string HashCode (ref int[] array, string word) {
+         array = new int [26];
+         foreach (char c in word) array[c - 65]++;
+         return string.Join ("", array);
       }
    }
 
