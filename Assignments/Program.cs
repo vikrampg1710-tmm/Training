407a2a51fc041f4a68c55cd8d461bcfd4c86148e
Vikram P G <Vikram.Gopinath@trumpf.com>
2024-01-24 11:49:17 +0530

    Review 1

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -1,3 +1,45 @@
 ﻿using System;
+using System.Numerics;
 
-Console.WriteLine("Hello, World!");
+namespace SparkTest;
+
+class Program {
+   public static void Main () {
+      Random rnd = new ();
+      Console.WriteLine ("Generating a random complex number: ");
+      var c = new ComplexNumber (rnd.Next(-100, 100), rnd.Next (-100, 100));
+      Console.WriteLine ($"x + iy = {c.RealPart} + ({c.ImaginaryPart})");
+      Console.WriteLine ($"Real Part: {c.RealPart}");
+      Console.WriteLine ($"Imaginary Part: {c.ImaginaryPart}");
+      Console.Write ($"Magnitude: {c.Norm (c)}");
+   }
+}
+
+/// <summary> Complex Number Class </summary>
+public class ComplexNumber {
+   private int real;
+   private int imaginary;
+   public ComplexNumber (int real, int imaginary) => (RealPart, ImaginaryPart) = (real, imaginary);
+
+   public int RealPart {
+      get => real;
+      set => real = value;
+   }
+
+   public int ImaginaryPart {
+      get => imaginary;
+      set => imaginary = value;
+   }
+
+   public double Norm (ComplexNumber a) {
+      var (r, i) = (a.real, a.imaginary);
+      return Math.Sqrt ((r*r) + (i*i));
+   }
+   public static ComplexNumber operator + (ComplexNumber a, ComplexNumber b) {
+      return new ComplexNumber (a.real + b.real, a.imaginary + b.imaginary);
+   }
+
+   public static ComplexNumber operator - (ComplexNumber a, ComplexNumber b) {
+      return new ComplexNumber (a.real - b.real, a.imaginary - b.imaginary);
+   }
+}
