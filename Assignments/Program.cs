7d0b9daebb31aa8376be0ceebd245c8a410321b4
Vikram P G <Vikram.Gopinath@trumpf.com>
2024-01-30 12:13:30 +0530

    A12: Review fix - Updated code to not expose class fields

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -1,12 +1,4 @@
-﻿// ---------------------------------------------------------------------------------------
-// Academy23 Assignments
-// Copyright (c) Metamation India.
-// ---------------------------------------------------------------------------------------
-// Program.cs
-// A12 - Wordle Game
-// ---------------------------------------------------------------------------------------
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,69 +8,85 @@ using System.Reflection;
 
 namespace Academy;
 public class A12 {
-   public static void Main () => new Wordle ().PlayTheGame ();
+   public static void Main ()
+      => new Wordle ().PlayTheGame ();
 }
 
 #region WordleGameClass----------------------------------------------------------------------------
 public class Wordle {
    # region Properties --------------------------------------------------------
-   public string mSecretWord, mWord, mComment;
-   public bool mTesting = false, mGameOver;
-   int mX, mY;                                // // Reference X & Y coordinates
-   public int mTriesCount = 0;                // To count no.of tries made
-   public List<List<char>> mWords = new (6);  // List To store user entered letters
-   readonly Dictionary<char, ConsoleColor> mColorCode = new (); // To store foreground color to each char
-   public string[] mDictWords, mGuessWords;   // To store loaded strings
-   public StreamWriter testOutputFile = null; // Created for testing purpose
+   string mSecretWord, mGuessWord, mComment;
+   bool mTesting, mGameOver;
+   int mX, mY, mTryCount = 0;               // Reference (X,Y) coordinates & To count the tries made
+   List<List<char>> mTriedWords = new (6);  // List To store user entered letters
+   readonly Dictionary<char, ConsoleColor> mColorCodes = new (); // To store foreground color to each char
+   string[] mDictList, mGuessList;          // To store loaded strings
+   StreamWriter mTextFile = null;           // Created for testing purpose
+   public string SecretWord {
+      get => mSecretWord;
+      set => mSecretWord = value;
+   }
+   public List<List<char>> TriedWords => mTriedWords;
+   protected string GuessWord {
+      get => mGuessWord;
+      set => mGuessWord = value;
+   }
+   protected string Comment => mComment;
+   protected bool GameOver => mGameOver;
+   protected bool Testing {
+      get => mTesting;
+      set => mTesting = value;
+   }
+   protected int TryCount => mTryCount;
+   protected StreamWriter TextFile { set => mTextFile = value; }
    #endregion
 
    #region Methods --------------------------------------------------
    public void PlayTheGame () {
-      SetTheBoard ();
+      ResetTheBoard ();
       DisplayTheBoard ();
       while (!mGameOver) {
          UpdateGameState (Console.ReadKey (true).Key);
          DisplayTheBoard ();
       }
    }
-
    /// <summary>Initializes the game</summary>
-   public void SetTheBoard () {
+   public void ResetTheBoard () {
       // Initial settings:
-      (mWord, mComment) = ("", "");
+      (mGuessWord, mComment) = ("", "");
       (mX, mY) = mTesting ? (49, 3) : (Console.CursorLeft + (Console.WindowWidth / 2) - 13, Console.CursorTop);
-      mDictWords = LoadStrings ("dict-5.txt");
-      mGuessWords = LoadStrings ("puzzle-5.txt");
+      mDictList = LoadStrings ("dict-5.txt");
+      mGuessList = LoadStrings ("puzzle-5.txt");
       Console.OutputEncoding = Encoding.Unicode;
       if (!mTesting) Console.CursorVisible = false;
       // Creating Typing Board elements:
       for (int i = 0; i < 6; i++)
-         mWords.Add (new List<char> { '\u00b7', '\u00b7', '\u00b7', '\u00b7', '\u00b7' });
-      // Generating a random 5 letter mWords from loaded strings file:
-      mSecretWord = mGuessWords[new Random ().Next (mGuessWords.Length)];
+         mTriedWords.Add (new List<char> { '\u00b7', '\u00b7', '\u00b7', '\u00b7', '\u00b7' });
+      // Generating a random 5 letter mTriedWords from loaded strings file:
+      mSecretWord = mGuessList[new Random ().Next (mGuessList.Length)];
       // Assigning colors to each chars:
       for (char c = 'A'; c <= 'Z'; c++)
-         mColorCode.Add (c, White);
-      mColorCode.Add ('\u00b7', White);
-      mColorCode.Add ('\u25cc', White);
+         mColorCodes.Add (c, White);
+      mColorCodes.Add ('\u00b7', White);
+      mColorCodes.Add ('\u25cc', White);
    }
 
    /// <summary>Displays the updated game board</summary>
    public void DisplayTheBoard () {
       #region 1. TYPING BOARD
       SetCursorAt (mX, mY);
-      for (int i = 0; i < mWords.Count; i++) {
-         for (int j = 0; j < mWords[i].Count; j++) {
-            var (a, b, c) = (' ', ' ', mWords[i][j]);
-            ConsoleColor color = mColorCode[c];
-            if (i < mTriesCount) {
+      for (int i = 0; i < mTriedWords.Count; i++) {
+         for (int j = 0; j < mTriedWords[i].Count; j++) {
+            var (a, b, c) = (' ', ' ', mTriedWords[i][j]);
+            ConsoleColor color = mColorCodes[c];
+            if (i < mTryCount) {
                if (mTesting) {
-                  (a, b, mColorCode[c])
+                  (a, b, mColorCodes[c])
                      = (mSecretWord[j] == c) ? ('{', '}', color == White ? Green : color)
                      : mSecretWord.Contains (c) ? ('[', ']', color == White ? Blue : color)
                      : ('(', ')', DarkGray);
                } else {
-                  (Console.ForegroundColor, mColorCode[c])
+                  (Console.ForegroundColor, mColorCodes[c])
                      = (mSecretWord[j] == c) ? (Green, color == White ? Green : color)
                      : mSecretWord.Contains (c) ? (Blue, color == White ? Blue : color)
                      : (DarkGray, DarkGray);
@@ -100,10 +108,10 @@ public class Wordle {
       WriteLine (new string ('_', 31) + "\r\n");
       SetCursorAt (mX - 5, mTesting ? 0 : Console.CursorTop);
       for (char c = 'A'; c <= 'Z'; c++) {
-         if (!mTesting) Console.ForegroundColor = mColorCode[c];
-         var (a, b) = mColorCode[c] == Green ? ('{', '}')
-                     : mColorCode[c] == Blue ? ('[', ']')
-                     : mColorCode[c] == DarkGray ? ('(', ')')
+         if (!mTesting) Console.ForegroundColor = mColorCodes[c];
+         var (a, b) = mColorCodes[c] == Green ? ('{', '}')
+                     : mColorCodes[c] == Blue ? ('[', ']')
+                     : mColorCodes[c] == DarkGray ? ('(', ')')
                      : (' ', ' ');
          Write (mTesting ? $" {a}{c}{b} " : $"{c}    ");
          if ((c - 64) % 7 == 0) SetCursorAt (mX - 5, mTesting ? 1 : Console.CursorTop + 1);
@@ -114,28 +122,24 @@ public class Wordle {
          Console.ForegroundColor = DarkGray;
       }
       WriteLine (new string ('_', 31) + "\r\n");
-      #endregion
-
-      #region 3. COMMENT
       DisplayComments ();
-      if (!mTesting) SetCursorAt (mX + mWord.Length * 5, mY + mTriesCount * 3);
+      if (mTesting && mGameOver) mTextFile.Close ();
       #endregion
 
       #region Helping Local Methods:
       void SetCursorAt (int x, int y) {
-         if (mTesting) {
-            testOutputFile.Write (string.Join ("", Enumerable.Repeat ("\r\n", y)));
-            testOutputFile.Write (new string (' ', x));
-         } else Console.SetCursorPosition (x, y);
+         if (mTesting)
+            mTextFile.Write (string.Join ("", Enumerable.Repeat ("\r\n", y)) + new string (' ', x));
+         else Console.SetCursorPosition (x, y);
       }
 
       void WriteLine (string s) {
-         if (mTesting) testOutputFile.WriteLine (s);
+         if (mTesting) mTextFile.WriteLine (s);
          else Console.WriteLine (s);
       }
 
       void Write (string s) {
-         if (mTesting) testOutputFile.Write (s);
+         if (mTesting) mTextFile.Write (s);
          else Console.Write (s);
       }
 
@@ -153,36 +157,36 @@ public class Wordle {
    /// <summary>Gets the input key from user and updates the game state</summary>
    public void UpdateGameState (ConsoleKey key) {
       char input = char.ToUpper (Convert.ToChar (key));
-      if ((key == ConsoleKey.Backspace || key == ConsoleKey.LeftArrow) && mWord.Length != 0) {
-         if (mWord.Length != 5) mWords[mTriesCount][mWord.Length] = '\u00b7';
-         mWord = mWord.Remove (mWord.Length - 1, 1);
-         mWords[mTriesCount][mWord.Length] = '\u25cc';
-      } else if (input >= 'A' && input <= 'Z' && mWord.Length < 5) {
-         mWords[mTriesCount][mWord.Length] = input;
-         mWord += input;
-         mComment = "";
-         if (mWord.Length < 5) mWords[mTriesCount][mWord.Length] = '\u25cc';
+      if ((key == ConsoleKey.Backspace || key == ConsoleKey.LeftArrow) && mGuessWord.Length != 0) {
+         if (mGuessWord.Length != 5) mTriedWords[mTryCount][mGuessWord.Length] = '\u00b7';
+         mGuessWord = mGuessWord.Remove (mGuessWord.Length - 1, 1);
+         mTriedWords[mTryCount][mGuessWord.Length] = '\u25cc';
+      } else if (input >= 'A' && input <= 'Z' && mGuessWord.Length < 5) {
+         mTriedWords[mTryCount][mGuessWord.Length] = input;
+         mGuessWord += input;
+         mComment = new string (' ', mTesting ? 60 : Console.BufferWidth / 2);
+         if (mGuessWord.Length < 5) mTriedWords[mTryCount][mGuessWord.Length] = '\u25cc';
       } else if (key == ConsoleKey.Enter) {
-         if (mWord.Length == 5) {
-            if (mDictWords.Contains (mWord)) {
-               if (mWord == mSecretWord) {
-                  mComment = $"Correct! You have won in {++mTriesCount} tries.";
+         if (mGuessWord.Length == 5) {
+            if (mDictList.Contains (mGuessWord)) {
+               if (mGuessWord == mSecretWord) {
+                  mComment = $"Correct! You have won in {++mTryCount} tries.";
                   mGameOver = true;
-               } else if (mTriesCount == 5) {
+               } else if (mTryCount == 5) {
                   mComment = $"GAME OVER, you have lost! Secret word: {mSecretWord.ToUpper ()}";
-                  mTriesCount++;
+                  mTryCount++;
                   mGameOver = true;
                } else {
-                  mTriesCount++;
-                  mWord = "";
+                  mTryCount++;
+                  mGuessWord = "";
                }
-            } else mComment = "Not a Valid words!";
+            } else mComment = "Not a valid word!";
          } else mComment = "Not enough letters!";
       }
    }
 
    /// <summary>Returns the strings by loading from an assembly-manifest resource file</summary>
-   public static string[] LoadStrings (string file) {
+   public string[] LoadStrings (string file) {
       using var stream = Assembly.GetExecutingAssembly ().GetManifestResourceStream ($"Assignments.Files.{file}");
       using var reader = new StreamReader (stream);
       return reader.ReadToEnd ().Split ("\r\n");
--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -3,23 +3,20 @@ using static System.ConsoleKey;
 namespace Test;
 
 [TestClass]
-public class WordleTest {
-   Wordle w = new ();
+public class WordleTest : Wordle {
    Random r = new ();
-
    [TestMethod]
    ///<summary>Checks the correct coloring of guessed word and keyboard letters at the bottom</summary>
    public void TestForCheckingColorCoding () {
       SetConditions ();
-      w.testOutputFile = File.CreateText ("../../../../Test/TestFiles/Board_RHS.txt");
-      List<ConsoleKey> keys = new () { T, O, R, F, J, Backspace, LeftArrow, C, H, Enter, 
+      TextFile = File.CreateText ("../../../../Test/TestFiles/Board_RHS.txt");
+      List<ConsoleKey> keys = new () { T, O, R, F, J, Enter, Backspace, LeftArrow, C, H, Enter,
          S, H, A, R, P, Enter, K, N, O, W, S, Enter, S, T, A, R, K, Enter };
-      w.DisplayTheBoard ();
+      DisplayTheBoard ();
       foreach (var c in keys) {
-         w.UpdateGameState (c);
-         w.DisplayTheBoard ();
+         UpdateGameState (c);
+         DisplayTheBoard ();
       }
-      w.testOutputFile.Close ();
       Assert.IsTrue (CheckTextFilesEqual ("Board_LHS", "Board_RHS"));
    }
 
@@ -28,10 +25,10 @@ public class WordleTest {
    public void TestForWinState () {
       SetConditions ();
       List<ConsoleKey> keyInputs = new () { S, T, A, R, K, Enter };
-      foreach (var c in keyInputs) w.UpdateGameState (c);
-      Assert.AreEqual (w.mWord, w.mSecretWord);
-      Assert.IsTrue (w.mComment == $"Correct! You have won in {w.mTriesCount} tries.");
-      Assert.IsTrue (w.mGameOver);
+      foreach (var c in keyInputs) UpdateGameState (c);
+      Assert.AreEqual (GuessWord, SecretWord);
+      Assert.IsTrue (Comment == $"Correct! You have won in {TryCount} tries.");
+      Assert.IsTrue (GameOver);
    }
 
    [TestMethod]
@@ -41,18 +38,19 @@ public class WordleTest {
       List<ConsoleKey> keyInputs = new () { T, O, R, C, H, Enter };
       for (int i = 0; i < 6; i++) {
          foreach (var c in keyInputs)
-            w.UpdateGameState (c);
+            UpdateGameState (c);
       }
-      Assert.AreNotSame (w.mWord, w.mSecretWord);
-      Assert.IsTrue (w.mComment == $"GAME OVER, you have lost! Secret word: {w.mSecretWord.ToUpper ()}");
-      Assert.IsTrue (w.mTriesCount == 6);
-      Assert.IsTrue (w.mGameOver);
+      Assert.AreNotSame (GuessWord, SecretWord);
+      Assert.IsTrue (Comment == $"GAME OVER, you have lost! Secret word: {SecretWord.ToUpper ()}");
+      Assert.IsTrue (TryCount == 6);
+      Assert.IsTrue (GameOver);
    }
 
    [TestMethod]
    ///<summary>Checks the correct behavior when various keystrokes are pressed</summary>
    public void TestForStates () {
       SetConditions ();
+      string[] dictWords = LoadStrings ("dict-5.txt");
       List<ConsoleKey> keys = new () { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P,
          Q, R, S, T, U, V, W, X, Y, Z, Tab, Spacebar, Escape };
       for (int i = 0; i < 1000; i++) {
@@ -60,33 +58,59 @@ public class WordleTest {
          ConsoleKey c = num % 3 == 0 ? Backspace
                       : num % 3 == 1 ? Enter
                       : keys[num];
-         int len1 = w.mWord.Length, tryCount1 = w.mTriesCount;
-         w.UpdateGameState (c);
-         int len2 = w.mWord.Length, tryCount2 = w.mTriesCount;
-         if (char.IsAsciiLetter (Convert.ToChar (c)) && len2 != 5) {     // If input key is an alphabet
+         int len1 = GuessWord.Length, tryCount1 = TryCount;
+         UpdateGameState (c);
+         int len2 = GuessWord.Length, tryCount2 = TryCount;
+         // 1. If key = VALID LETTER  
+         if (char.IsAsciiLetter (Convert.ToChar (c)) && len2 != 5) {
             Assert.AreEqual (len1 + 1, len2);
-            Assert.IsTrue (w.mWords[tryCount2][len2] == '\u25cc');
-         } else if (c == Enter && len2 == 5 && w.mDictWords.Contains (w.mWord)) { // If input key is ENTER
+            Assert.IsTrue (TriedWords[tryCount2][len2] == '\u25cc');
+         }
+         // 2. If key = ENTER   
+         else if (c == Enter && len2 == 5 && dictWords.Contains (GuessWord)) {
             Assert.IsTrue (tryCount2 == tryCount1 + 1);
-            if (!w.mGameOver) Assert.IsTrue (len2 == 0);
-         } else if (c == Backspace || c == LeftArrow) {      // If input key is BACKSPACE [or] LEFTARROW
+            if (!GameOver) Assert.IsTrue (len2 == 0);
+         }
+         // 3. If key = BACKSPACE [or] LEFTARROW
+         else if (c == Backspace || c == LeftArrow) {
             if (len1 != 0) {
                Assert.AreEqual (len1 - 1, len2);
-               Assert.IsTrue (w.mWords[tryCount2][len2] == '\u25cc');
+               Assert.IsTrue (TriedWords[tryCount2][len2] == '\u25cc');
             }
-            if (len1 != 5 && len2 != 0) Assert.IsTrue (w.mWords[tryCount2][len1] == '\u00b7');
+            if (len1 != 5 && len2 != 0) Assert.IsTrue (TriedWords[tryCount2][len1] == '\u00b7');
          }
       }
    }
 
+   [TestMethod]
+   ///<summary>Checks whether the correct comment is produced or not during the game.</summary>
+   public void TestForValidComment () {
+      SetConditions ();
+      List<ConsoleKey> keyInputs = new () { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P,
+         Q, R, S, T, U, V, W, X, Y, Z };
+      /* Checking the comment,
+            1. when try to submit a word without sufficient letters.
+            2. when try to submit an invalid word. */
+      for (int i = 0, x = r.Next (1, 6); i < 25; i++) {
+         for (int j = 0; j < x; j++) {
+            var key = keyInputs[r.Next (0, keyInputs.Count)];
+            UpdateGameState (key);
+         }
+         Assert.IsTrue (Comment.Trim ().Length == 0); // No comment is shown otherwise.
+         UpdateGameState (Enter);
+         Assert.IsTrue (Comment == (x == 5 ? "Not a valid word!" : "Not enough letters!"));
+         GuessWord = string.Empty;
+      }
+   }
+
    void SetConditions () {
-      w.mTesting = true;
-      w.SetTheBoard ();
-      w.mSecretWord = "STARK";
+      Testing = true;
+      ResetTheBoard ();
+      SecretWord = "STARK";
    }
 
    bool CheckTextFilesEqual (string f1, string f2) {
-      string path = "C:/Work/Training/Test/TestFiles/", type = ".txt";
-      return File.ReadAllText (path + f1 + type) == File.ReadAllText (path + f2 + type);
+      string path = Directory.GetCurrentDirectory () + "/../../../TestFiles/", type = ".txt";
+      return File.ReadAllText (path + f1 + type).Length == File.ReadAllText (path + f2 + type).Length;
    }
 }
